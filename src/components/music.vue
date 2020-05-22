46bb3f1821a054a57bc494a81045a9e65b029817
lkfun <a@lkfun.cc>
2020-05-22 11:36:27 +0800

    change music

--- src/components/music.vue
+++ src/components/music.vue
@@ -1,7 +1,7 @@
 <template>
   <div >
     <div class="music" v-bind:style="styleObject">
-      <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=298 height=52 src="//music.163.com/outchain/player?type=2&id=1311319145&auto=0&height=32"></iframe>
+      <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=298 height=52 src="//music.163.com/outchain/player?type=2&id=1309903595&auto=0&height=32"></iframe>
       <span v-on:click="musicmethod">♬</span>
     </div>
   </div>
