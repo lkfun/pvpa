7b45442a8cf3962a66cd89f434153b0fe99762f9
lkfun <a@lkfun.cc>
2020-05-22 10:47:30 +0800

    replace  yeshouw with chouyou

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,21 +1,19 @@
 <template>
   <div class="hello">
-    <div class="yeshouwarp yeshou">
-      <div  class="yeshoumask yeshou"></div>
-      <div class="yeshou yeshou-blur" v-bind:style="yeshou"></div>
+    <div class="yeshou yeshouwarp ">
+      <div class="yeshou " v-bind:style="yeshou"></div>
     </div>
-    <h1>{{ msg }}</h1>
+    <h1><del>{{ msg }}</del></h1>
     <p>QQ群:<a :href="qqgroupadd[0]" target="_blank">{{ qqgroup[0] }}</a></p>
     <p>QQ群:<a :href="qqgroupadd[1]" target="_blank">{{ qqgroup[1] }}</a></p>
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: '欢 迎 来 到 新 宝 岛',
+      msg: '欢迎来到阿斯特莱亚大陆（误）',
       qqgroup: ['199938498', '453620381'],
       qqgroupadd: ['https://jq.qq.com/?_wv=1027&k=5LUqzJ0', 'https://jq.qq.com/?_wv=1027&k=5mdqJjQ'],
       isActive: true,
@@ -24,7 +22,7 @@ export default {
         top: '270px'
       },
       yeshou: {
-        backgroundImage: 'url(' + require('../assets/yeshou.jpg') + ')'
+        backgroundImage: 'url(' + require('../assets/chouyou.gif') + ')'
       }
     }
   }
@@ -36,7 +34,7 @@ export default {
     font-weight: normal;
   }
   .yeshoumask{
-    background:rgba(0, 0, 0, 0.3);
+    background:rgba(0, 0, 0, 0.5);
     position: absolute;
     z-index: 100;
   }
@@ -45,14 +43,17 @@ export default {
     margin: 0 auto;
   }
   .yeshou{
-    width:399px;
+    width:300px;
     height: 300px;
     border-radius: 12px;
+    background-repeat:no-repeat;
+    background-size:100% 100%;
+    -moz-background-size:100% 100%;
   }
   .yeshou-blur{
     filter: blur(10px);
   }
   h1{
-    color: #a53015;
+    color: #f54b8c;
   }
 </style>
